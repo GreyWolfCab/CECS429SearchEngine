23503a4e62c44b59e9a1e6abbf22a187b6e1d22a
mattszeto <ms8288@gmail.com>
2020-09-21 13:05:39 -0700

    completed tokenprocessor implementation

--- src/cecs429/text/AdvancedTokenProcesser.java
+++ src/cecs429/text/AdvancedTokenProcesser.java
@@ -1,30 +1,68 @@
 package cecs429.text;
+
 import org.tartarus.snowball.ext.englishStemmer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AdvancedTokenProcesser implements TokenProcessor{
 
 
     @Override
-    public String processToken(String token) {
+    public ArrayList<String> processToken(String token) {
 
-        //remove all non-alphanumeric characters from the beginning and end
+        int beginIndex = 0;
+        int endIndex = 0;
+        String currentToken;
+        ArrayList<String> result = new ArrayList<String>();
 
+        // Remove all non-alphanumeric characters from the beginning and end of token
+        String[] stringArray = token.split("");
 
+            // Parse through string array to find beginning index
+        for(int i = 0; i < stringArray.length;i++){
+            if(stringArray[i].matches("^[a-zA-Z0-9]*$")){
+                beginIndex = i;
+                break;
+            }
+        }
 
-        //remove all apostrophes or quotation marks (single or double) from anywhere in the string
+            // Parse through string array to find ending index
+        for(int i = stringArray.length - 1; i > 0;i--){
+            if(stringArray[i].matches("^[a-zA-Z0-9]*$")){
+                endIndex = i;
+                break;
+            }
+        }
 
+            // Change array to only include selected indices
+        stringArray = Arrays.copyOfRange(stringArray, beginIndex, endIndex + 1);
+        currentToken = String.join("", stringArray);
 
+        // Remove all apostrophes or quotation marks (single or double) from anywhere in the string
+        currentToken = currentToken.replaceAll("\'","");
+        currentToken = currentToken.replaceAll("\"","");
 
-        //remove hyphens and split up the original hyphenated token into multiple tokens (returns combined, and separated strings)
+        // Remove hyphens and split up the original hyphenated token into multiple tokens (returns combined, and separated strings)
+        stringArray = currentToken.split("-",-1);
+        currentToken = currentToken.replaceAll("-","");
 
+        // Convert token to lowercase and add to result array
+        result.add(currentToken.toLowerCase());
+        for(int i = 0; i < stringArray.length;i++){
+            result.add(stringArray[i].toLowerCase());
+        }
 
-        //convert token to lowercase
-
-
-        //stem token using implementation of the Porter2stemmer
+        // Stem token using implementation of the Porter2stemmer
 
         englishStemmer stemmer = new englishStemmer();
+        for(int i = 0; i < result.size(); i++){
+            stemmer.setCurrent(result.get(i));
+            if(stemmer.stem()){
+                result.set(i, stemmer.getCurrent());
+            }
+        }
 
-        return null;
+        return result;
     }
 }
--- src/cecs429/text/TokenProcessor.java
+++ src/cecs429/text/TokenProcessor.java
@@ -1,11 +1,14 @@
 package cecs429.text;
 
+import java.util.ArrayList;
+
 /**
  * A TokenProcessor applies some rules of normalization to a token from a document, and returns a term for that token.
  */
 public interface TokenProcessor {
 	/**
 	 * Normalizes a token into a term.
-	 */
-	String processToken(String token);
+     * @return
+     */
+	ArrayList<String> processToken(String token);
 }
